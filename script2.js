57067e7d622b2d74e8b6733f4e2eb973dc73567d
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2022-04-10 23:42:45 +0300

    Add_Calculator_V02

--- script2.js
+++ script2.js
@@ -1,4 +1,5 @@
-
+let a
+a = prompt("Lutfen islem seciniz:  (+ , / )")
 
 
 function topla(sayi1="" , sayi2=""){
@@ -9,4 +10,21 @@ function topla(sayi1="" , sayi2=""){
 }
 
 
-topla()
+function bol(sayi1="" , sayi2=""){
+    sayi1 = prompt("Ilk sayiyi girin:")
+    sayi2 = prompt("Ikinci sayiyi girin:")
+    let sonuc = Number(sayi1) / Number(sayi2)
+    console.log(sonuc)
+}
+
+if( a == "+"){
+    let islem1 = document.querySelector('#toplama')
+    islem1.innerHTML = topla()
+}
+else if( a == "/"){
+    let islem2 = document.querySelector('#bolme')
+    islem2.innerHTML = bol()
+}
+else{
+    console.log("Lutfen gecerli bir islem seciniz.")
+}
