a8bcda8cd22198a3d2e3775c246036e1e143a310
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2022-04-10 23:34:56 +0300

    Add_Calculator_V01

---
+++ script2.js
@@ -0,0 +1,12 @@
+
+
+
+function topla(sayi1="" , sayi2=""){
+    sayi1 = prompt("Ilk sayiyi girin")
+    sayi2 = prompt("Ikinci sayiyi girin")
+    let sonuc = Number(sayi1) + Number(sayi2)
+    console.log(sonuc)
+}
+
+
+topla()
